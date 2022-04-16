110cbd58bb284d48f4fa7285a9581b5139da59bb
hwangdaehan <hwangdaehan@dogmaster.fun>
2022-04-16 17:37:18 +0900

    asasdsadsad

---
+++ src/main/java/com/pig94/pig94/Pig94Application.java
@@ -0,0 +1,13 @@
+package com.pig94.pig94;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pig94Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pig94Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pig94/pig94/Pig94ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pig94.pig94;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pig94ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
